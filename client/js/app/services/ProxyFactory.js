a259a2a1b38fbd4780239a70553080dcfbf3cfde
Lucas Oliveira <luksrn@gmail.com>
2018-03-20 22:35:40 -0300

    03.11 - Correções no Proxy

--- client/js/app/services/ProxyFactory.js
+++ client/js/app/services/ProxyFactory.js
@@ -9,18 +9,19 @@ class ProxyFactory {
                         && typeof(target[prop]) == typeof(Function) ){
 
                     return function() {
-                        Reflect.apply(target[prop],target, arguments);
-                        return acao(target);
+                        let retorno = Reflect.apply(target[prop],target, arguments);
+                        acao(target);
+                        return retorno;
                     }
                 }
                 return Reflect.get(target,prop,receiver);
             },
             set(target,prop,value,receiver){                
+                let retorno = Reflect.set(target,prop,value,receiver);
                 if(props.includes(prop)){
-                    target[prop] = value;
                     acao(target);
                 }
-                return Reflect.set(target,prop,value,receiver);
+                return retorno;
             }
 
         });
